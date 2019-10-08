87d1524681edda4042e38367933dcd05f49ccc9c
Andrew McDermott <aim@frobware.com>
2019-10-08 08:40:51 +0100

    Remove old metrics

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -8,92 +8,18 @@ var (
 	routesReachableGauge = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "route_state",
-			Help: "Current state of a route.",
+			Help: "Reachability state of a route.",
 		},
 		[]string{"name", "reachable", "unreachable", "unknown"},
 	)
-
-	routesReachableHistogram = prometheus.NewHistogramVec(
-		prometheus.HistogramOpts{
-			Name: "route_reachability",
-			Help: "Number of reachable routes.",
-		},
-		[]string{"name", "status"},
-	)
-
-	routesReachableCounter = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "routes_reachable_total",
-			Help: "Number of reachable routes.",
-		},
-		[]string{"name"},
-	)
-
-	routesUnReachableCounter = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "routes_unreachable_total",
-			Help: "Number of unreachable routes.",
-		},
-		[]string{"name"},
-	)
-
-	routesUnknownCounter = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "routes_unknown_total",
-			Help: "Number of unknown routes.",
-		},
-		[]string{"name"},
-	)
 )
 
-func SetRoutesReachableGauge(name string) {
+func SetRouteReachable(name string) {
 	routesReachableGauge.With(prometheus.Labels{"name": name, "reachable": "1", "unreachable": "0", "unknown": "0"}).Set(1)
 }
 
-func SetRoutesUnreachableGauge(name string) {
-	routesReachableGauge.With(prometheus.Labels{"name": name, "reachable": "0", "unreachable": "1", "unknown": "0"}).Set(1)
-}
-
-func IncRoutesReachableCounter(name string) {
-	routesReachableCounter.With(prometheus.Labels{"name": name}).Inc()
-}
-
-func IncRoutesUnreachableCounter(name string) {
-	routesUnReachableCounter.With(prometheus.Labels{"name": name}).Inc()
-}
-
-func IncRoutesUnknownCounter(name string) {
-	routesUnknownCounter.With(prometheus.Labels{"name": name}).Inc()
-}
-
-func SetRouteStatus(name string, status string) {
-	routesReachableHistogram.WithLabelValues(name, status).Observe(1.0)
-}
-
-func SetRouteReachable(name string) {
-	// Experimenting with metric types; what makes most sense here?
-
-	// Historgram?
-	SetRouteStatus(name, "200")
-
-	// Counter?
-	IncRoutesReachableCounter(name)
-
-	// Gauge?
-	SetRoutesReachableGauge(name)
-}
-
 func SetRouteUnreachable(name string) {
-	// Experimenting with metric types; what makes most sense here?
-
-	// Histogram?
-	SetRouteStatus(name, "404")
-
-	// Counter?
-	IncRoutesUnreachableCounter(name)
-
-	// Gauge?
-	SetRoutesUnreachableGauge(name)
+	routesReachableGauge.With(prometheus.Labels{"name": name, "reachable": "0", "unreachable": "1", "unknown": "0"}).Set(1)
 }
 
 func SetRouteUnknown(name string) {
@@ -101,10 +27,6 @@ func SetRouteUnknown(name string) {
 }
 
 func init() {
-	prometheus.MustRegister(routesReachableCounter)
-	prometheus.MustRegister(routesUnReachableCounter)
-	prometheus.MustRegister(routesUnknownCounter)
 	prometheus.MustRegister(routesReachableGauge)
-	prometheus.MustRegister(routesReachableHistogram)
 	prometheus.MustRegister(prometheus.NewBuildInfoCollector())
 }
